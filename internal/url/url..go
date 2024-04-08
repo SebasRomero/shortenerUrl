af83a8465e8e3925d984ab772a789a9291645bfb
Sebastian <sebastian@coderlabs.co>
2024-04-07 22:58:55 -0400

    feat:: add ConvertToDecimal

---
+++ internal/url/url..go
@@ -0,0 +1,32 @@
+package functionalities
+
+import (
+	"math"
+)
+
+var COUNTER = 100000000
+
+const base62Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func ConvertToBase62(number int) string {
+	base62 := ""
+	for number > 0 {
+		remainder := number % 62
+		base62 = string(base62Digits[remainder]) + base62
+		number /= 62
+	}
+	return base62
+}
+
+func ConvertToDecimal(encode string) int {
+	result := 0.0
+	for i, value_i := range encode {
+		for j, value_j := range base62Digits {
+			if string(value_i) == string(value_j) {
+				value := len(encode) - i - 1
+				result += (math.Pow(62, float64(value)) * float64(j))
+			}
+		}
+	}
+	return int(result)
+}
