838f7f175491fb2af4cc9877cdfed421865759ef
Sebastian <sebastian@coderlabs.co>
2024-04-09 18:08:35 -0400

    feat:: add self check to remove expired urls

--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/sebasromero/shortenerUrl/internal/database"
 	"github.com/sebasromero/shortenerUrl/internal/server"
 )
+
+var mainConnection = database.Connection
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	go func() {
+		for range time.Tick(time.Minute * 10) {
+			mainConnection.RemoveExpiredShortUrls()
+		}
+	}()
+
 	fmt.Println("Listen at:", port)
 	http.ListenAndServe(":"+port, server.InitRoutes())
 }
